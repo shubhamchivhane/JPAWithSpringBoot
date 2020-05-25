e8f13f7c520933c7ea9e6f03e88667b354d7aa6e
91738 <91738@192.168.0.7>
2020-05-25 14:29:50 +0530

    my first commit and push

---
+++ MYREC/src/main/java/com/shubham/MYREC/MyrecApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.MYREC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrecApplication.class, args);
+	}
+
+}
---
+++ MYREC/src/test/java/com/shubham/MYREC/MyrecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.MYREC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
